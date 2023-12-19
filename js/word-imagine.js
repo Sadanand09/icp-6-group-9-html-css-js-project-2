6d66987663f64f63b437392ab647eb6a5bba970f
kartikkotkar <kartikkotkar973@gmail.com>
2023-12-18 21:24:48 -0800

    add code javascript

--- js/word-imagine.js
+++ js/word-imagine.js
@@ -11,7 +11,7 @@ const words = [
     "vaishnavi",
     "ganesh"
 ];
- 
+
 const hints = [
    
     "imagine a correct name",
@@ -27,3 +27,47 @@ const hints = [
     "imagine a correct name",
     
 ];
+
+
+let displayWord = "";
+
+
+function shuffle(str) {
+    strArray = Array.from(str);
+    for (let i = 0; i < strArray.length - 1; ++i) {
+        let j = Math.floor(Math.random() * strArray.length);
+        
+        let temp = strArray[i];
+        strArray[i] = strArray[j];
+        strArray[j] = temp;
+    }
+    return strArray.join(" ");
+} 
+
+
+function check() {
+    let input = document.getElementById("input");
+    let output = document.getElementById("output");
+    if (
+        input.value.toLocaleLowerCase() ===
+        displayWord.toLocaleLowerCase()
+    )
+        output.innerHTML = "Result: Congragulation";
+    else output.innerHTML = "Result: Incorrect";
+}
+
+function refresh() {
+    index = Math.floor(Math.random() * 11);
+    displayWord = words[index];
+    displayHint = hints[index];
+    scrambleWord =
+        document.getElementById("scrambleWord");
+    scrambleWord.innerText =
+       shuffle(displayWord).toUpperCase();
+    let hint = document.getElementById("hint");
+    hint.innerHTML = "<b>Hint:</b> " + displayHint;
+    document.getElementById("output").innerText = "Result:";
+}
+
+ 
+refresh();
