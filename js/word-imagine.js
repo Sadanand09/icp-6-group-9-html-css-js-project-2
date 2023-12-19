a1057b68628bee60d6742f73daee19c122a48ca5
kartikkotkar <kartikkotkar973@gmail.com>
2023-12-18 19:23:21 -0800

    adedd  javascript code

---
+++ js/word-imagine.js
@@ -0,0 +1,29 @@
+const words = [
+    "kartik",
+    "pratik",
+    "sagar",
+    "om",
+    "kalyani",
+    "rutik",
+    "sadanand",
+    "pooja",
+    "vaidehi",
+    "vaishnavi",
+    "ganesh"
+];
+ 
+const hints = [
+   
+    "imagine a correct name",
+    "imagine a correct name",
+    "imagine a correct name",
+    "imagine a correct name",
+    "imagine a correct name",
+    "imagine a correct name",
+    "imagine a correct name",
+    "imagine a correct name",
+    "imagine a correct name",
+    "imagine a correct name",
+    "imagine a correct name",
+    
+];
