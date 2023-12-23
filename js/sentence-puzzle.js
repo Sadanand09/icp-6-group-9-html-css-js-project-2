7b45991d725b1af20a95d8bdd6c531b08877a055
kartikkotkar <kartikkotkar973@gmail.com>
2023-12-22 23:02:18 -0800

    create a sentence puzzle game and create .js file & .html fle .css file

---
+++ js/sentence-puzzle.js
@@ -0,0 +1,63 @@
+const words = [
+    "A man was preparing the pitch for a cricket match",
+    "santosh is going to play cricket",
+    "wellcome to my farmhouse",
+    "It is too much like a cricket match",
+    "sham is go to play footboll",
+];
+
+
+const hints = [
+   
+    "A man was preparing the pitch for a cricket match",
+    "santosh is going to play cricket",
+    "wellcome to my farmhouse",
+    "It is too much like a cricket match",
+    "sham is go to play footboll",
+    
+];
+
+
+let displayWord = "";
+
+
+function shuffle(str) {
+    strArray = Array.from(str);
+    for (let i = 0; i < strArray.length - 1; ++i) {
+        let j = Math.floor(Math.random() * strArray.length);
+       
+        let temp = strArray[i];
+        strArray[i] = strArray[j];
+        strArray[j] = temp;
+    }
+    return strArray.join(" ");
+}
+
+
+function check() {
+    let input = document.getElementById("input");
+    let output = document.getElementById("output");
+    if (
+        input.value.toLocaleLowerCase() ===
+        displayWord.toLocaleLowerCase()
+    )
+        output.innerHTML = "Result: Congragulation";
+    else output.innerHTML = "Result: Incorrect";
+}
+
+
+function refresh() {
+    index = Math.floor(Math.random() * 5);
+    displayWord = words[index];
+    displayHint = hints[index];
+    scrambleWord =
+        document.getElementById("scrambleWord");
+    scrambleWord.innerText =
+       shuffle(displayWord).toUpperCase();
+    let hint = document.getElementById("hint");
+    hint.innerHTML = "<b>correct👇:</b> " + displayHint;
+    document.getElementById("output").innerText = "Result:";
+}
+
+
+refresh();
